b931ff0c8bbcf621d1565aac3433801fa3376f77
Camilla Rygh <camillahallo@mail.com>
2019-02-05 07:19:33 +0100

    Extract answers and answer options

--- quiz.js
+++ quiz.js
@@ -5,8 +5,12 @@ function random(max) {
 }
 
 function makeQuiz(set, title) {
+    document.getElementById('header1').innerHTML = title
+    document.getElementById('header2').innerHTML = "Det finnes " + set.length + " spørsmål i denne kategorien. Du vil få maksimalt 20 spørsmål"
+    //add possibility to choose amount of question, show available amount
     const elements = getRandomElements(questions, set)
-    document.getElementById('main-block').innerHTML = "<h1>" + title + "</h1>"
+    document.getElementById('options').innerHTML = "<button id=\"btn\">Start quiz</button>"
+    document.getElementById('btn').addEventListener('click', function() {startQuiz(elements)})
     console.log(elements)
     console.log(title)
 }
@@ -31,3 +35,26 @@ function getRandomElements(amount, lst) {
         return internal(amount, [], lst.length - 1, lst)
     }
 }
+
+function startQuiz(elements) {
+    function internal(correct) {
+        if (elements.length === 0) {
+            endQuiz(correct)
+        } else {
+            current = elements.pop()
+            document.getElementById('header2').innerHTML = current.question
+            makeOptionDiv(current.answers)
+            document.getElementById('btn').innerHTML = "Neste"
+            //change caller function or introduce global counter
+        }
+    }
+    internal(0)
+}
+
+function makeOptionDiv(answers) {
+    Object.keys(answers).forEach(function(key) {
+        console.log(key)
+        console.log(answers[key])
+    })
+}
+
